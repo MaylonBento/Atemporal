f741e734bd4031badd5e28c51adb45bb539836fb
Focxer <you@example.com>
2023-04-10 21:31:51 -0300

    Bruno

--- assets/cadastro.php
+++ assets/cadastro.php
@@ -109,18 +109,18 @@
                         <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
                     </svg><span style="color: white;">Login</button>
                 <ul class="dropdown-menu dropdown-menu-end dropdown-menu-dark">
-                    <li><a class="dropdown-item" href="./assets/minhaConta.php">
+                    <li><a class="dropdown-item" href="minhaConta.php">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-fill me-1" viewBox="0 0 16 16">
                                 <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5Z" />
                                 <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6Z" />
                             </svg>Minha Conta</a></li>
 
-                    <li><a class="dropdown-item" href="./assets/purchases.php">
+                    <li><a class="dropdown-item" href="purchases.php">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill me-1" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
                             </svg>Meus Pedidos</a></li>
 
-                    <li><a class="dropdown-item" href="./assets/support.php">
+                    <li><a class="dropdown-item" href="support.php">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-question-circle-fill me-1" viewBox="0 0 16 16">
                                 <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.496 6.033h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286a.237.237 0 0 0 .241.247zm2.325 6.443c.61 0 1.029-.394 1.029-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94 0 .533.425.927 1.01.927z" />
                             </svg>Atendimento</a></li>
--- assets/iframe/meusProdutos.php
+++ assets/iframe/meusProdutos.php
@@ -16,6 +16,9 @@
     <!-- CSS do Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
+    <!-- CSS dos Produtos -->
+    <link rel="stylesheet" href="../css/meusProdutos.css">
+
     <!-- Nome da Página -->
     <title>Meus Produtos - ATEMPORAL</title>
 
@@ -29,72 +32,51 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@300;400&family=Inter:wght@400;700&display=swap" rel="stylesheet">
 </head>
-<style>
-    *{
-        margin: 0;
-        padding: 0;
-        font-family: 'Inter', sans-serif;
-        color: gray;
-        box-sizing: border-box;
-        scroll-behavior: smooth;
-    }
 
-    body{
-        height: 100vh;
-        width: 100%;
-
-        background-color: transparent;
-    }
-
-    main{
-        height: auto;
-        width: 100%;
-    }
-
-    .meus-dados-bg{
-        width: 100%;
-        height: auto;
-    }
-
-    form{
-        height: auto;
-        width: 100%;
-
-        display: flex;
-        justify-content: space-between;
-        gap: 40px;
-    }
-
-    .foto-usuario{
-        width: 26%;
-        height: auto;
-
-        overflow: hidden;
-    }
-
-    .dados-usuario{
-        width: 100%;
-        height: auto;
-
-        overflow: hidden;
-    }
-
-    img{
-        border: 1px solid #ddd;
-        border-radius: 4px;
-        padding: 5px;
-        width: 140px;
-        height: 140px;
-        object-fit: cover;
-        margin-bottom: 10px;
-    }
-</style>
 <body>
     <main>
         <div class="meus-produtos-bg">
-            <h1>Meus Produtos</h1>
+            <div class="botoes-bg">
+                <div class="botoes" id="novoAnuncio">
+                        <ul>
+                            <li>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M480 32c0-12.9-7.8-24.6-19.8-29.6s-25.7-2.2-34.9 6.9L381.7 53c-48 48-113.1 75-181 75H192 160 64c-35.3 0-64 28.7-64 64v96c0 35.3 28.7 64 64 64l0 128c0 17.7 14.3 32 32 32h64c17.7 0 32-14.3 32-32V352l8.7 0c67.9 0 133 27 181 75l43.6 43.6c9.2 9.2 22.9 11.9 34.9 6.9s19.8-16.6 19.8-29.6V300.4c18.6-8.8 32-32.5 32-60.4s-13.4-51.6-32-60.4V32zm-64 76.7V240 371.3C357.2 317.8 280.5 288 200.7 288H192V192h8.7c79.8 0 156.5-29.8 215.3-83.3z"/></svg>                            </li>
+                            <li>
+                                <span>Novo Anúncio</span>
+                                <p>Crie até 3 anúncios para desapegar rapidinho!</p>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="botoes" id="removerAnuncio">
+                        <ul>
+                            <li>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32V240c0 8.8-7.2 16-16 16s-16-7.2-16-16V64c0-17.7-14.3-32-32-32s-32 14.3-32 32V336c0 1.5 0 3.1 .1 4.6L67.6 283c-16-15.2-41.3-14.6-56.6 1.4s-14.6 41.3 1.4 56.6L124.8 448c43.1 41.1 100.4 64 160 64H304c97.2 0 176-78.8 176-176V128c0-17.7-14.3-32-32-32s-32 14.3-32 32V240c0 8.8-7.2 16-16 16s-16-7.2-16-16V64c0-17.7-14.3-32-32-32s-32 14.3-32 32V240c0 8.8-7.2 16-16 16s-16-7.2-16-16V32z"/></svg>                            </li>
+                            <li>
+                                <span>Encerrar Anúncio</span>
+                                <p>Ja vendeu ou deseja encerrar o seu anúncio?</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="lista-produtos"></div>
         </div>
     </main>
     
 </body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+<script>
+    const novoAnuncio = document.getElementById('novoAnuncio');
+    const removerAnuncio = document.getElementById('removerAnuncio');
+
+    novoAnuncio.addEventListener('click', ()=>{
+        window.location.href = "./redirect/novoAnuncio.php";
+    })
+
+    removerAnuncio.addEventListener('click', ()=>{
+        window.location.href = "./redirect/removerAnuncio.php";
+    })
+</script>
 </html>
