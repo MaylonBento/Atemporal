981175dd2847e19c58ffca9174dcdee448d78e9e
Focxer <you@example.com>
2023-05-09 23:29:33 -0300

    Número de Antigos para 10

--- assets/routines/index/antigosModal.php
+++ assets/routines/index/antigosModal.php
@@ -1,7 +1,7 @@
 <?php
 include_once '../connection.php';
 
-$stmAnuncios = $connection->prepare("SELECT*FROM TB_ANUNCIO ORDER BY ID_ANUNCIO ASC LIMIT 5");
+$stmAnuncios = $connection->prepare("SELECT*FROM TB_ANUNCIO ORDER BY ID_ANUNCIO ASC LIMIT 10");
 if ($stmAnuncios->execute()) {
     $res = $stmAnuncios->get_result();
     $row = $res->fetch_all(MYSQLI_ASSOC);
